a30764acf7581b8d22c6d3706d48211c77c554cc
shahriaarrr <shahriaarrr@gmail.com>
2024-10-27 16:56:27 +0330

    work with Arrays

---
+++ 3/03_Arrays.py
@@ -0,0 +1,10 @@
+numbers = [1, 3, 5, 6, 9, 12, 18]
+
+print(numbers) # [1, 3, 5, 6, 9, 12, 18]
+
+for i in numbers:
+    print(i)
+    # print each element of the Array
+
+for i in range(0, len(numbers)):
+    print(numbers[i])
---
+++ 3/exersise/4-Array.py
@@ -0,0 +1,8 @@
+numbers = [3, 1, 3, 1, 2, 7]
+
+for i in numbers:
+    if i % 2 ==0:
+        print(f"{i} % 2 = 0")
+    
+    else:
+        print(f"{i} % 2 != 0")
