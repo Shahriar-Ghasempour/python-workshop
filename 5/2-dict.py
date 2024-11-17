e3dc325f6622c4b34cefa7eb9e4caaf6c7e90b4c
shahriaarrr <shahriaarrr@gmail.com>
2024-11-17 16:45:12 +0330

    work with dics

---
+++ 5/2-dict.py
@@ -0,0 +1,11 @@
+user = {
+    "username": "shahriar",
+    "password": "123456",
+    "email": "shahriaarrr@gmail.com",
+    "phone": "9092301202"
+}
+
+print (type(user))
+print (user)
+
+print(user["username"])
---
+++ 5/exersise/2.py
@@ -0,0 +1,11 @@
+lesson = {
+    "name": "python workshop",
+    "department": "Computer Engineer",
+    "unit": "3",
+    "teacher": "Matin Hosseinpour",
+    "semester": "1",
+    "year": "1403",
+}
+
+for item in lesson:
+    print(f"{item}: {lesson[item]}")
